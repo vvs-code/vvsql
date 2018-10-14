9a5826fbc546b33b7aaa826dc3912dc7aeff4f9b
vvs-code <vlad@veselov.spb.ru>
2018-10-14 10:24:40 +0300

    Добавил типы данных

--- i.py
+++ i.py
@@ -1,9 +1,9 @@
 filename = 'types' # input()
+# Получение БД
 file = open(filename + '.vvsql', 'r')
 responseText = ''.join(file.read().split('\n'))
 file.close()
-# Получение БД
-writeFile = open(filename + '.vvsql', 'w')
+# writeFile = open(filename + '.vvsql', 'w')
 # Разделение н астроки
 linedResponseText = responseText.split('%%n%%')
 # Создание массива данных и заголовков
@@ -12,15 +12,48 @@ resultArrayHeaders = []
 # Первая строка отвечает за заголовки, остальные за данные
 isHeaders = True
 # Добавление данных в массив данных
-for i in linedResponseText:
+for i in range(len(linedResponseText)):
 	if isHeaders:
-		headers = i.split('%%d%%')
+		headers = linedResponseText[i].split('%%d%%')
 		for j in headers:
-
-		isHeaders = False
+			headerArray = j.split('^^')
+			resultArrayHeaders += [[headerArray[0], headerArray[1]]]
 		headers = None
+		isHeaders = False
 	else:
-		resultArray += [i.split('%%d%%')]
+		data = linedResponseText[i].split('%%d%%')
+		print('data: ' + str(data))
+		resultArray += [[]]
+		for j in range(len(data)):
+			headerType = resultArrayHeaders[j][0]
+			if headerType == 'b':
+				resultArray[i - 1] += [not not int(data[j])]
+			elif headerType == 'i':
+				resultArray[i - 1] += [int(data[j])]
+			elif headerType == 's':
+				resultArray[i - 1] += [data[j]]
+			elif headerType == 'n':
+				resultArray[i - 1] += [None]
+			elif headerType == 'bn' or headerType == 'in' or headerType == 'sn':
+				if data[j].split('^^')[0] == 'n':
+					resultArray[i - 1] += [None]
+				else:
+					if headerType == 'bn':
+						resultArray[i - 1] += [not not int(data[j])]
+					elif headerType == 'in':
+						resultArray[i - 1] += [int(data[j])]
+					elif headerType == 'sn':
+						resultArray[i - 1] += [data[j]]
+			elif headerType == 'm':
+				if data[j].split('^^')[0] == 'b':
+					resultArray[i - 1] += [not not int(data[j].split('^^')[1])]
+				elif data[j].split('^^')[0] == 'i':
+					resultArray[i - 1] += [int(data[j].split('^^')[1])]
+				elif data[j].split('^^')[0] == 's':
+					resultArray[i - 1] += [data[j].split('^^')[1]]
+				elif data[j].split('^^')[0] == 'n':
+					resultArray[i - 1] += [None]
+print(resultArray)
 # Создание массива команд-методов
 cmdArray = ['open']
 while cmdArray[0] != 'close':
@@ -89,10 +122,13 @@ while cmdArray[0] != 'close':
 		if cmdArray[0] != 'close':
 			print('err: Команда ' + cmdArray[0] + ' не найдена')
 # Формировение респонса и отправление в БД
-headersToWrite = '%%d%%'.join(resultArrayHeaders)
-contentsToWrite = []
-for i in resultArray:
-	contentsToWrite += ['%%d%%'.join(i)]
-responseToWrite = headersToWrite + '%%n%%' + '%%n%%'.join(contentsToWrite)
-writeFile.write(responseToWrite)
-print('--- closed and saved')
+# resultArrayHeadersToWrite = []
+# for i in resultArrayHeaders:
+# 	resultArrayHeadersToWrite += ['^^'.join(i)]
+# headersToWrite = '%%d%%'.join(resultArrayHeadersToWrite)
+# contentsToWrite = []
+# for i in resultArray:
+# 	contentsToWrite += ['%%d%%'.join(i)]
+# responseToWrite = headersToWrite + '%%n%%' + '%%n%%'.join(contentsToWrite)
+# writeFile.write(responseToWrite)
+# print('--- closed and saved')
